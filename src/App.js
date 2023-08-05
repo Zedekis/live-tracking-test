1a523b247d34dd47382ca6db55b6267d531f73b9
Edris Zoghlami <edris.zoghlami@gmail.com>
2023-08-05 14:56:48 -0400

    switched from real time database to firestore database, added query

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { useMemo, useState, useEffect } from "react";
-import db from "./firebase_setup/firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import database from "./firebase_setup/firebase";
 import { getDatabase, ref, onValue } from "firebase/database";
 import car from "./images/car.png";
 import {
@@ -19,12 +20,24 @@ export default function App() {
   const [currentLocation, setCurrentLocation] = useState([]);
   const center = useMemo(() => ({ lat: 45.44437, lng: -73.82018 }), []);
 
+  // const fetchdata = async () => {
+  //   const starCountRef = ref(db, "Location/");
+  //   onValue(starCountRef, (snapshot) => {
+  //     const data = snapshot.val();
+  //     setCurrentLocation(data);
+  //     console.log(data);
+  //   });
+  // };
+
   const fetchdata = async () => {
-    const starCountRef = ref(db, "Location/");
-    onValue(starCountRef, (snapshot) => {
-      const data = snapshot.val();
-      setCurrentLocation(data);
-      console.log(data);
+    const q = query(
+      collection(database, "/users"),
+      where("Name", "==", "Edris")
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      setCurrentLocation(doc.data().Location);
     });
   };
 
--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,16 +9,16 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyDyF1vY8t71FBaQeXgzpSYXofk1uyjMeEw",
   authDomain: "gps-test-project-b13a0.firebaseapp.com",
-  databaseURL: "https://gps-test-project-b13a0-default-rtdb.firebaseio.com",
+  //databaseURL: "https://gps-test-project-b13a0-default-rtdb.firebaseio.com",
   projectId: "gps-test-project-b13a0",
   storageBucket: "gps-test-project-b13a0.appspot.com",
   messagingSenderId: "920038043246",
   appId: "1:920038043246:web:e09854d1951c5c504ec07f",
-  measurementId: "G-1DC66C8JTH",
+  //measurementId: "G-1DC66C8JTH",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const database = getFirestore(app);
 
 export default database;
